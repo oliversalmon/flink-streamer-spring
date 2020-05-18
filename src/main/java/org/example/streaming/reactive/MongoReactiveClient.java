07850ed7d88a758183d50a2a609cfede332d4542
dineshgpillai <dinesh.g.pillai@gmail.com>
2020-05-18 22:00:15 +0100

    Initial set of classes checked in

---
+++ src/main/java/org/example/streaming/reactive/MongoReactiveClient.java
@@ -0,0 +1,28 @@
+package org.example.streaming.reactive;
+
+
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+
+@EnableReactiveMongoRepositories
+public class MongoReactiveClient
+        extends AbstractReactiveMongoConfiguration {
+
+    @Bean
+    public MongoClient mongoClient() {
+        return MongoClients.create();
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return "TWITTER";
+    }
+
+    @Override
+    public MongoClient reactiveMongoClient() {
+        return null;
+    }
+}
---
+++ src/main/java/org/example/streaming/reactive/repository/Tweet.java
@@ -0,0 +1,8 @@
+package org.example.streaming.reactive.repository;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Tweet {
+
+}
