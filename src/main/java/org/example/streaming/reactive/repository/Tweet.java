6cec7ea5ff347b9a83e73cde2cbbcbb74a8a770a
dineshgpillai <dinesh.g.pillai@gmail.com>
2020-05-19 21:17:23 +0100

    Repo classes checked in

--- src/main/java/org/example/streaming/reactive/repository/Tweet.java
+++ src/main/java/org/example/streaming/reactive/repository/Tweet.java
@@ -1,8 +1,18 @@
 package org.example.streaming.reactive.repository;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Getter
+@Setter
 @Document
 public class Tweet {
 
+
+    private String id_str;
+    private String  text;
+
 }
+
+
---
+++ src/main/java/org/example/streaming/reactive/repository/User.java
@@ -0,0 +1,11 @@
+package org.example.streaming.reactive.repository;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+
+    private String screen_name;
+}
